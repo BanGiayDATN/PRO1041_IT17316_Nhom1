9d7eba43f9b5783c835a7caa9788e416d8885efd
daoxuandu <108521350+daoxuandu@users.noreply.github.com>
2022-11-16 20:22:35 +0700

    Du Cập nhật Tìm kiếm BH

--- src/main/java/com/mycompany/ungdungbanlaptop/view/viewBaoHanh/TimKiemBaoHanh.java
+++ src/main/java/com/mycompany/ungdungbanlaptop/view/viewBaoHanh/TimKiemBaoHanh.java
@@ -119,7 +119,7 @@ public class TimKiemBaoHanh extends javax.swing.JPanel {
                     .addComponent(txt_SoHoaDonDo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(30, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
