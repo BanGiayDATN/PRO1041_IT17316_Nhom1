43efa2b8d9521c166aa1de0ccef382bb114608c0
thangdtph27626 <109157942+thangdtph27626@users.noreply.github.com>
2022-11-17 21:34:08 +0700

    chirnh sửa lại code

--- src/main/java/com/mycompany/ungdungbanlaptop/view/AdQuanLiHang.java
+++ src/main/java/com/mycompany/ungdungbanlaptop/view/AdQuanLiHang.java
@@ -20,10 +20,12 @@ import javax.swing.table.DefaultTableModel;
 public class AdQuanLiHang extends javax.swing.JPanel {
 
     private HangService hangService = new HangServiceImpl();
+    private List<Hang> listHang = hangService.getList();
+    private int index = 0;
     
     public AdQuanLiHang() {
         initComponents();
-        loadTable(hangService.getList());
+        loadTable(listHang);
     }
 
     private void loadTable(List<Hang> list){
@@ -220,9 +222,10 @@ public class AdQuanLiHang extends javax.swing.JPanel {
             }else{
                 errorMa.setText("");
                 hangService.insert(hang);
+                listHang.add(hang);
             }
         }
-        loadTable(hangService.getList());
+        loadTable(listHang);
     }//GEN-LAST:event_btnThemActionPerformed
 
     private void btnSuaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSuaActionPerformed
@@ -234,15 +237,18 @@ public class AdQuanLiHang extends javax.swing.JPanel {
             Hang hang = hangRequest();
             UUID id = UUID.fromString(txtId.getText());
                 hangService.update(id,hang);
+                listHang.set(index, hang);
+            
         }
-        loadTable(hangService.getList());
+        loadTable(listHang);
     }//GEN-LAST:event_btnSuaActionPerformed
 
     private void tblHangMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblHangMouseClicked
-        int index = tblHang.getSelectedRow();
+         index = tblHang.getSelectedRow();
         txtId.setText(tblHang.getModel().getValueAt(index, 0).toString());
         txtMa.setText(tblHang.getModel().getValueAt(index, 1).toString());
         txtTen.setText(tblHang.getModel().getValueAt(index, 2).toString());
+        System.out.println(txtId.getText());
     }//GEN-LAST:event_tblHangMouseClicked
 
 
